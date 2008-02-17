5aa995d923da3064b263e0051dfeb6fff950bed9
hayfordoleary <hayfordoleary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-17 09:25:43 +0000

    Adding in old versions
    git-svn-id: https://plugins.svn.wordpress.org/oceia-bar/tags/2.1.1@32042 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oceia-bar.php
+++ oceia-bar.php
@@ -1,9 +1,9 @@
 <?php
 /*
 	Plugin Name: Oceia bar
-	Plugin URI: http://archivemedia.org/oceia/bar/
+	Plugin URI: http://sdho.org/oceia/bar/
 	Description: A simple administration bar, inspired by WordPress&#8217; admin interface.
-	Version: 2.3.1
+	Version: 2.1.1
 	Author: Sean Hayford O&#8217;Leary
 	Author URI: http://sdho.org/
 */
@@ -41,40 +41,36 @@ if(get_bloginfo('version') >= '2.1') {
 				$oceiabar_edit = '';
 			}
 		}
-		echo "\n<div id=\"oceiabar\">\n\t<div id=\"oceiabar-quicklinks\">\n\t\t<ul id=\"oceiabar-quicklinks-list\">\n".$oceiabar_account."\n".$oceiabar_dashboard."\n".$oceiabar_new."\n".$oceiabar_edit."\n\t</ul></div>\n<div id=\"oceiabar-bloginfo\">&#8220;".wptexturize(get_bloginfo('name'))."&#8221;</div>\n</div>\n";
+		echo "\n<div id=\"oceiabar\">\n\t<div id=\"oceiabar-quicklinks\">\n\t\t<ul>\n".$oceiabar_account."\n".$oceiabar_dashboard."\n".$oceiabar_new."\n".$oceiabar_edit."\n\t</ul></div>\n<div id=\"oceiabar-bloginfo\">&#8220;".wptexturize(get_bloginfo('name'))."&#8221;</div>\n</div>\n";
 	}}
 
-	function oceiabar_css() {
+	function oceiabar_style() {
 		if(current_user_can('read')) {
-			$oceiabar_css = "<style type=\"text/css\" media=\"all\">\n\t/* <![CDATA[ */\n\tbody {\n\t\tpadding: 28px 0 0 0 !important;\n\t}\n\n\t#oceiabar {\n\t\tbackground: #14568a url('".get_settings('siteurl')."/wp-content/plugins/oceia-bar/background.png') center top no-repeat !important;\n\t\tborder-bottom: 1px solid #3285ae !important;\n\t\tcolor: #c3def1 !important;\n\t\tfont: 12px \"lucida grande\", \"lucida sans unicode\", \"tahoma\", sans-serif !important;\n\t\tfont-style: normal !important;\n\t\theight: 27px !important;\n\t\tleft: 0 !important;\n\t\tletter-spacing: 0 !important;\n\t\tposition: absolute !important;\n\t\ttop: 0 !important;\n\t\twidth: 100% !important;\n\t}\n\n\t#oceiabar #oceiabar-bloginfo {\n\t\tmargin: 0 !important;\n\t\tpadding: 0 !important;\n\t\tposition: absolute !important;\n\t\tright: 1em !important;\n\t\ttop: 7px !important;\n\t}\n\n\t#oceiabar #oceiabar-bloginfo #name {\n\t\tbackground: inherit !important;\n\t\tcolor: #fff !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks ul {\n\t\tpadding: 0 !important;\n\t\tmargin: 0 !important;\n\t\tlist-style: none !important;\n\t\ttext-align: left !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li {\n\t\tfloat: left !important;\n\t\tposition: relative !important;\n\t\twidth: 9em !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a.oceiabar-primary {\n\t\tbackground: url('".get_settings('siteurl')."/wp-content/plugins/oceia-bar/down.png') right center no-repeat !important;\n\t\tpadding-right: 5px !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li ul {\n\t\tbackground: #14568a !important;\n\t\tborder:1px solid #3285ae !important;\n\t\tcolor: inherit !important;\n\t\tdisplay: none !important;\n\t\tposition: absolute !important;\n\t\ttop: 1em !important;\n\t\tleft: 0 !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li ul li a {\n\t\ttext-align: left !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li ul {\n\t\ttop: auto !important;\n\t\tleft: auto !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li:hover ul, #oceiabar #oceiabar-quicklinks li.over ul {\n\t\tdisplay: block !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks ul {\n\t\tlist-style: none !important;\n\t\tmargin: 0 !important;\n\t\tpadding: 0 !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a {\n\t\tbackground: inherit !important;\n\t\tcolor: #c3def1 !important;\n\t\tdisplay: block !important;\n\t\tfont-weight: normal !important;\n\t\tpadding: .5em 10px !important;\n\t\ttext-align: center !important;\n\t\ttext-decoration: none !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a.oceiabar-primary {\n\t\ttext-align: left !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li:hover, #oceiabar #oceiabar-quicklinks li a:hover {\n\t\tbackground: #2c77a4 !important;\n\t\tcolor: #fff !important;\n\t\ttext-decoration: none !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a.oceiabar-primary:hover {\n\t\tbackground: #2c77a4 url('".get_settings('siteurl')."/wp-content/plugins/oceia-bar/down.png') right center no-repeat !important;\n\t\tcolor: #fff !important;\n\t\tpadding-right: 5px !important;\n\t\ttext-decoration: none !important;\n\t}";
+			$oceiabar_style = "<style type=\"text/css\">\n\t/* <![CDATA[ */\n\tbody {\n\t\tpadding: 28px 0 0 0 !important;\n\t}\n\n\t#oceiabar {\n\t\tbackground: #14568a url('".get_settings('siteurl')."/wp-content/plugins/oceia-bar/background.png') center top no-repeat !important;\n\t\tborder-bottom: 1px solid #3285ae !important;\n\t\tcolor: #c3def1 !important;\n\t\tfont: 12px \"lucida grande\", \"lucida sans unicode\", \"tahoma\", sans-serif !important;\n\t\tfont-style: normal !important;\n\t\theight: 27px !important;\n\t\tleft: 0 !important;\n\t\tletter-spacing: 0 !important;\n\t\tposition: absolute !important;\n\t\ttop: 0 !important;\n\t\twidth: 100% !important;\n\t}\n\n\t#oceiabar #oceiabar-bloginfo {\n\t\tmargin: 0 !important;\n\t\tpadding: 0 !important;\n\t\tposition: absolute !important;\n\t\tright: 1em !important;\n\t\ttop: 7px !important;\n\t}\n\n\t#oceiabar #oceiabar-bloginfo #name {\n\t\tbackground: inherit !important;\n\t\tcolor: #fff !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks {\n\t\tpadding: 0 !important;\n\t\tmargin: 0 !important;\n\t\tlist-style: none !important;\n\t\ttext-align: left !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li {\n\t\tfloat: left !important;\n\t\tposition: relative !important;\n\t\twidth: 9em !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a.oceiabar-primary {\n\t\tbackground: url('".get_settings('siteurl')."/wp-content/plugins/oceia-bar/down.png') right center no-repeat !important;\n\t\tpadding-right: 5px !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li ul {\n\t\tbackground: #14568a !important;\n\t\tborder:1px solid #3285ae !important;\n\t\tcolor: inherit !important;\n\t\tdisplay: none !important;\n\t\tposition: absolute !important;\n\t\ttop: 1em !important;\n\t\tleft: 0 !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li ul li a {\n\t\ttext-align: left !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li>ul {\n\t\ttop: auto !important;\n\t\tleft: auto !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li:hover ul, #oceiabar #oceiabar-quicklinks li.over ul {\n\t\tdisplay: block !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks ul {\n\t\tlist-style: none !important;\n\t\tmargin: 0 !important;\n\t\tpadding: 0 !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a {\n\t\tbackground: inherit !important;\n\t\tcolor: #c3def1 !important;\n\t\tdisplay: block !important;\n\t\tfont-weight: normal !important;\n\t\tpadding: .5em 10px !important;\n\t\ttext-align: center !important;\n\t\ttext-decoration: none !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a.oceiabar-primary {\n\t\ttext-align: left !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li:hover, #oceiabar #oceiabar-quicklinks li a:hover {\n\t\tbackground: #2c77a4 !important;\n\t\tcolor: #fff !important;\n\t\ttext-decoration: none !important;\n\t}\n\n\t#oceiabar #oceiabar-quicklinks li a.oceiabar-primary:hover {\n\t\tbackground: #2c77a4 url('".get_settings('siteurl')."/wp-content/plugins/oceia-bar/down.png') right center no-repeat !important;\n\t\tcolor: #fff !important;\n\t\tpadding-right: 5px !important;\n\t\ttext-decoration: none !important;\n\t}";
 
 			if(file_exists(dirname(__FILE__).'/style.css') && is_readable(dirname(__FILE__).'/style.css')) {
 				$oceiabar_customstyle = file_get_contents(dirname(__FILE__).'/style.css');
 				if(!empty($oceiabar_customstyle)) {
-					$oceiabar_css = str_replace(' !important', '', $oceiabar_css)."\n\n".$oceiabar_customstyle;
+					$oceiabar_style = str_replace(' !important', '', $oceiabar_style)."\n\n".$oceiabar_customstyle;
 				}
 			}
 
-			$oceiabar_css .= "\n\t/* ]]> */\n</style>\n<style type=\"text/css\" media=\"print\">\n\t/* <![CDATA[ */\n\tbody {;\n\t\tpadding: 0 !important;\n\t}\n\n\t#oceiabar {\n\t\tdisplay: none;\n\t}\n\t/* ]]> */\n</style>\n";
-			echo $oceiabar_css;
+			$oceiabar_style .= "\n\t/* ]]> */\n</style>\n";
+			echo $oceiabar_style;
 		}
 	}
 
 	function oceiabar_js() {
 		if(current_user_can('read')) {
-			echo "<script type=\"text/javascript\">\n\t<!-- // <![CDATA[ -->\n\toceiaBarDrop = function() {\n\t\tif (document.all&&document.getElementById) {\n\t\t\tnavRoot = document.getElementById(\"oceiabar-quicklinks-list\");\n\t\t\tfor (i=0; i<navRoot.childNodes.length; i++) {\n\t\t\t\tnode = navRoot.childNodes[i];\n\t\t\t\tif (node.nodeName == \"LI\") {\n\t\t\t\t\tnode.onmouseover=function() {\n\t\t\t\t\t\tthis.className+=\" over\";\n\t\t\t\t\t}\n\t\t\t\t\tnode.onmouseout=function() {\n\t\t\t\t\t\tthis.className=this.className.replace(\" over\", \"\");\n\t\t\t\t\t}\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n\twindow.onload = oceiaBarDrop;\n\t<!-- // ]]> -->\n</script>\n";
-		}
-	}
-	
-	if(!function_exists('wp_adminbar')) {
-		function wp_adminbar() {
-			return TRUE;
+			echo "<script type=\"text/javascript\">\n\t// <![CDATA[\n\tstartList = function() {\n\t\tif (document.all&&document.getElementById) {\n\t\t\tnavRoot = document.getElementById(\"oceiabar\").getElementById(\"oceiabar-quicklinks\");\n\t\t\tfor (i=0; i<navRoot.childNodes.length; i++) {\n\t\t\t\tnode = navRoot.childNodes[i];\n\t\t\t\tif (node.nodeName==\"LI\") {\n\t\t\t\t\tnode.onmouseover=function() {\n\t\t\t\t\t\tthis.className+=\" over\";\n\t\t\t\t\t}\n\t\t\t\t\tnode.onmouseout=function() {\n\t\t\t\t\t\tthis.className=this.className.replace(\" over\", \"\");\n\t\t\t\t\t}\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n\twindow.onload=startList;\n\t// ]]>\n</script>\n";
 		}
 	}
 
-	add_action('wp_head', 'oceiabar_js');
-	add_action('wp_head', 'oceiabar_css');
-	add_action('wp_footer', 'oceiabar');
+	if($_GET['preview'] !== 'true') {
+		add_action('wp_head', 'oceiabar_js');
+		add_action('wp_head', 'oceiabar_style');
+		add_action('wp_footer', 'oceiabar');
+	}
 } else {
 	$oceiabar_plugin_path = ABSPATH.'wp-content'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR;
 	$oceiabar_plugin = str_replace($oceiabar_plugin_path, '', __FILE__);
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-=== Oceia bar ===Contributors: hayfordolearyTags: adminRequires at least: 2.1Tested up to: 2.3.1Stable tag: 2.3.1
+=== Oceia bar ===Contributors: hayfordolearyTags: adminRequires at least: 2.1Tested up to: 2.2Stable tag: 2.1.1
 
 It's a vex of us all: we're reading our posts, need to change something, but how do we get to the administration? That's what Oceia bar is all about.
 
@@ -6,11 +6,8 @@ It's a vex of us all: we're reading our posts, need to change something, but how
 It's a peeve of us all: we're reading our posts, decide we want to add something new, but how do we get to the administration? In the default theme, there's a link, but we certainly don't all use that. That's what Oceia bar is for -- it provides quick, visible access to the admin for just to basic Dashboard, to editing a specific post, or adding a new one.In fact you can basically try this out on [WordPress.com](http://wordpress.com) if you have an account. (Oceia bar is based on the WordPress.com bar's functionality -- the bar WP.com uses is not distributed to the public.)
 
 == Updates ==
-As of 2007/12/06:
+As of 2007/03/17:
 
-* Drop-downs on Oceia bar 2.3 and earlier did not work on Internet Explorer 6. This incompatibility has been fixed; quick thanks to user Adam for reporting the issue.
-* Oceia Bar 2.3 contains a new function called `wp_adminbar()`. This doesn't do much for regular users, but it's useful for theme designers, who can use `function_exists()` to see if Oceia bar is installed and hide the theme's "Edit this entry" text. I'm trying to get other admin bars to implement this same function, too.
-* Oceia bar 2.1.1 contained a filter that is no longer necessary in WordPress 2.2. This version is still compatible with 2.1, though.
 * Oceia bar 2.0 had a smart filter to detect what directory it was being placed in (for displaying CSS images). Unfortunately, this wasn't functioning properly on Windows Server. Anyway, this has been removed, so you *must* upload the entire `oceia-bar` directory if you want the bar to look right.
 * This version is almost identical to 2.0.9, but all legacy WordPress 2.0.x code has been removed. Thus, this version is only compatible with WordPress 2.1+.
 * This README has been updated to the [new WordPress README standards](http://wordpress.org/extend/plugins/about/#readme).
@@ -37,5 +34,5 @@ Oceia bar is released under the GNU General Public License. See [http://gnu.org/
 
 = Uggh! My bar is showing up in the wrong place! =This can be an issue in some themes, since certain positions in CSS can override Oceia bar's position: absolute;. Generally you can resolve this by making sure `wp_footer();` is not contained within any elements other than the `<body>`.
 
-= I `position: absolute;` my links and they don't work when I have Oceia bar turned on! =Oceia bar can be problematic if you use `position: absolute;` to control the location of hyperlinks or other content. This occurs because Oceia's default CSS puts a 30px padding on the `<body>`, so most content is shoved down 30px but your `position; absolute;` content is not. Fortunately, it's relatively easy to resolve (though Oceia bar will cover up your top 30px of content):1. Create a file called `style.css`2. Add the following: `body { margin: 0; padding: 0; }`3. Upload `style.css` to your `oceia-bar` directory on `wp-content/plugins`.= This thing is ugly. How do I make it not ugly? =Pretty much the same the previous question:1. Create a file called `style.css`2. Add whatever CSS you want. Note that any CSS you put will take precedence over Oceia"s default CSS.3. Upload `style.css` to your oceia-bar directory on `wp-content/plugins`.= I found a bug!/Why doesn't it do {x}?/I have an idea to improve Oceia bar =[Tell me about it](http://sdho.org/contact/).
+= I "position: absolute;" my links and they don't work when I have Oceia bar turned on! =Oceia bar can be problematic if you use `position: absolute;` to control the location of hyperlinks or other content. This occurs because Oceia's default CSS puts a 30px padding on the `<body>`, so most content is shoved down 30px but your `position; absolute;` content is not. Fortunately, it's relatively easy to resolve (though Oceia bar will cover up your top 30px of content):1. Create a file called `style.css`2. Add the following: `body { margin: 0; padding: 0; }`3. Upload `style.css` to your `oceia-bar` directory on `wp-content/plugins`.= This thing is ugly. How do I make it not ugly? =Pretty much the same the previous question:1. Create a file called `style.css`2. Add whatever CSS you want. Note that any CSS you put will take precedence over Oceia"s default CSS.3. Upload `style.css` to your oceia-bar directory on `wp-content/plugins`.= I found a bug!/Why doesn't it do {x}?/I have an idea to improve Oceia bar =[Tell me about it](http://sdho.org/contact/).
 = My question isn't answered. That's rude. =Again, [tell me about it](http://sdho.org/contact/).
